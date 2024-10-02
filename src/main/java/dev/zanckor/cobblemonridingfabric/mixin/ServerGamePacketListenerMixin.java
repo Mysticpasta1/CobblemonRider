6043299b393fe3c5965bbcbb35abdc38ae0c59aa
Zanckor <zanckor002@gmail.com>
2024-10-02 12:21:28 +0200

    Fixed boats glitching movement

--- src/main/java/dev/zanckor/cobblemonridingfabric/mixin/ServerGamePacketListenerMixin.java
+++ src/main/java/dev/zanckor/cobblemonridingfabric/mixin/ServerGamePacketListenerMixin.java
@@ -1,15 +1,23 @@
 package dev.zanckor.cobblemonridingfabric.mixin;
 
 
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
+import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerGamePacketListenerMixin {
+    @Shadow public ServerPlayerEntity player;
+
     @ModifyConstant(method = "onVehicleMove", constant = @Constant(doubleValue = 0.0625D))
-    private double modifyValue(double originalValue) {
-        return 1;
+    private double modifyValue(double constant) {
+        boolean isPokemon = this.player.getVehicle() instanceof PokemonEntity;
+
+        return isPokemon ? 3.0D : 0.0625D;
     }
 }
